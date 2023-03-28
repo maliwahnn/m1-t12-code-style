46e88c3ef2cc0832d4c0e1689b482ce0a337c752
maliwahn <lucky777men@mail.ru>
2023-03-28 19:45:29 +0500

    Исправлена работа

---
+++ src/CalculateDeposit.java
@@ -0,0 +1,58 @@
+
+import java.util.Scanner;
+
+public class CalculateDeposit {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new CalculateDeposit().
+            do_important_job();
+    }
+
+    double calculateComplexPercentFunction(double a, double y, int d) {
+       double pay = a * Math.pow((1 + y / 12), 12 * d);
+
+       return round(pay, 2);
+    }
+
+    double calculateSimplePercentFunction(double doubleAmount, double double_year_rate,
+                                             int deposit_period) {
+
+         return round(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+    }
+
+    double round(double value, int places) {
+        double ScaLe = Math.pow(10, places);
+
+       return Math.round(value * ScaLe) / ScaLe;
+    }
+
+    void do_important_job() {
+        int period;
+        int action;
+        double outDoubleVar = 0;
+
+        System.out.println("Введите сумму вклада в рублях:");
+        int amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt( );
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        if (action == 1) {
+            outDoubleVar = calculateSimplePercentFunction(amount, 0.06, period);
+
+        } else if (action == 2)  {
+            outDoubleVar = calculateComplexPercentFunction(amount, 0.06, period);
+        }
+
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outDoubleVar);
+    }
+
+
+
+
+
+}
